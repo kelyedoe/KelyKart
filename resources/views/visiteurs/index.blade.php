654dccb353fb477d7bf845769bc7aa6b6cab94a9
kelyedoe <edoeagbodjana@gmail.com>
2023-01-29 22:17:59 +0100

    Developper la section footer de l'accueil utilisateur

--- resources/views/visiteurs/index.blade.php
+++ resources/views/visiteurs/index.blade.php
@@ -199,11 +199,40 @@
                     </a>
                 </div>
             </div>
-        </div>   
-        
+        </div>      
         
     </section>
 {{-- Fin de section des categories --}}
+
+{{-- Debut du footer --}}
+
+<section class="footer row">
+    
+        <div class="footer__retrouvez">
+            <div class="footer__retrouvez--header">Retrouvez moi</div>
+            <div class="footer__retrouvez--content"><p>
+                Après avoir appris sur Internet et dans les livres quoi de plus normal que de partager à son tour ? Passionné par le web depuis un peu plus de 3 ans maintenant j'aime partager mes compétences et mes découvertes avec les personnes qui ont cette même passion pour le web</p></div>
+            <div class="footer__retrouvez--medias">
+                <div><ion-icon name="logo-linkedin" class="footer__retrouvez--icon"></ion-icon></div>
+                <div><ion-icon name="logo-octocat"  class="footer__retrouvez--icon"></ion-icon></div>
+                <div><ion-icon name="logo-youtube" class="footer__retrouvez--icon"></ion-icon></div>
+                <div><ion-icon name="logo-instagram" class="footer__retrouvez--icon"></ion-icon></div>
+            </div>
+        </div>
+        <div class="footer__contactez">
+            <div class="footer__logo">
+                <img src="{{asset('images/kelykart.png')}}" class="footer__contactez--logo" alt="logo footer'">
+            </div>
+            <div class="footer__contactez--header">Contactez moi</div>
+            <div class="footer__contactez--content"><p>Je vous reponds dès que possible , Merci!</p></div>
+            <div class="footer__contactez--medias">
+                <div><ion-icon name="logo-linkedin" class="footer__retrouvez--icon"></ion-icon></div>
+                <div><ion-icon name="mail-unread-outline" class="footer__retrouvez--icon"></ion-icon></div>  
+            </div>
+        </div>
+    
+    
+</section>
 
 
 
