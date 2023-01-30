7615071ebf5319838dd4123afbd4cd9c3aa2a746
kelyedoe <edoeagbodjana@gmail.com>
2023-01-30 13:06:39 +0100

    ajouter la page des formations

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('visiteurs.content');
 });
+
+Route::get('formations', function(){
+    return view('visiteurs.formation-page');
+});
